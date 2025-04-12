e6d8b61d55269910cf8d028b28018994a95247f5
VPatel5 <55804219+VPatel5@users.noreply.github.com>
2025-04-11 21:38:29 -0400

    implement a console

---
+++ server/src/main/java/me/vpatel/console/ServerConsole.java
@@ -0,0 +1,48 @@
+package me.vpatel.console;
+
+import net.minecrell.terminalconsole.SimpleTerminalConsole;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+
+public class ServerConsole extends SimpleTerminalConsole {
+
+    private static final Logger log = LogManager.getLogger(ServerConsole.class);
+
+    @Override
+    public void start() {
+        new Thread(super::start, "TerminalThread").start();
+    }
+
+    @Override
+    protected boolean isRunning() {
+        return true;
+    }
+
+    @Override
+    protected void runCommand(String s) {
+        try {
+            executeCommand(s);
+        } catch (Exception ex)
+        {
+            log.error("Error occurred executing command {}", s, ex);
+        }
+    }
+
+    public void executeCommand(String s)
+    {
+
+    }
+
+    @Override
+    protected LineReader buildReader(LineReaderBuilder builder)
+    {
+        return super.buildReader(builder.appName("ChatApplication"));
+    }
+    @Override
+    protected void shutdown() {
+        log.info("Shutting down");
+        System.exit(0);
+    }
+}
--- server/src/main/java/me/vpatel/server/ChatServer.java
+++ server/src/main/java/me/vpatel/server/ChatServer.java
@@ -1,8 +1,36 @@
 package me.vpatel.server;
 
+import me.vpatel.console.ServerConsole;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ChatServer {
 
-    public static void main(String[] args) {
+    private static final Logger log = LogManager.getLogger(ChatServer.class);
 
+    private final int port;
+
+    private ServerConsole console;
+
+    public ChatServer(int port)
+    {
+        this.port = port;
+    }
+
+    public static void main(String[] args)
+    {
+        log.info("Starting up Chat Server");
+
+        ChatServer chatServer = new ChatServer(8080);
+        chatServer.start();
+    }
+
+    public void start()
+    {
+        log.info("Starting up Server Console");
+        console = new ServerConsole();
+        console.start();
+        log.info("Console Started");
     }
 }
+
