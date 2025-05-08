1430753be9509ae79a9c91513a0c57495c174ace
VPatel5 <55804219+VPatel5@users.noreply.github.com>
2025-05-08 00:25:20 -0400

    implement clientlistreqpacket

--- server/src/main/java/me/vpatel/server/ConvoServerHandler.java
+++ server/src/main/java/me/vpatel/server/ConvoServerHandler.java
@@ -4,18 +4,12 @@ import me.vpatel.network.ConvoConnection;
 import me.vpatel.network.api.ConvoUser;
 import me.vpatel.network.protocol.ConvoHandler;
 import me.vpatel.network.protocol.ConvoPacket;
-import me.vpatel.network.protocol.client.ClientEncryptionResponsePacket;
-import me.vpatel.network.protocol.client.ClientLoginStartPacket;
-import me.vpatel.network.protocol.client.ClientPingPacket;
-import me.vpatel.network.protocol.client.ClientRegisterRequestPacket;
+import me.vpatel.network.protocol.client.*;
 import me.vpatel.network.protocol.server.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Base64;
-import java.util.Objects;
-import java.util.Queue;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 
@@ -126,6 +120,33 @@ public class ConvoServerHandler extends ConvoHandler {
             log.info("Received ping packet from {} with payload {}", connection.getRemoteAddress(), clientPingPacket.getPayload());
             connection.sendPacket(new ServerPongPacket(clientPingPacket.getPayload()));
         }
+        else if (msg instanceof ClientListRequestPacket packet) {
+            switch (packet.getType()) {
+                case FALCUN_USERS ->
+                        connection.sendPacket(new ServerListResponsePacket(getUsers(), true));
+                case INCOMING_FRIEND_INVITES ->
+                        connection.sendPacket(new ServerListResponsePacket(packet.getType(), server.getFriendsHandler().getIncomingInvites(connection.getUser())));
+                case OUTGOING_FRIEND_INVITES ->
+                        connection.sendPacket(new ServerListResponsePacket(packet.getType(), server.getFriendsHandler().getOutgoingInvites(connection.getUser())));
+                case INCOMING_GROUP_INVITES ->
+                        connection.sendPacket(new ServerListResponsePacket(packet.getType(), server.getGroupsHandler().getIncomingInvites(connection.getUser())));
+                case OUTGOING_GROUP_INVITES ->
+                        connection.sendPacket(new ServerListResponsePacket(packet.getGroupName(), server.getGroupsHandler().getOutgoingInvites(packet.getGroupName(), connection.getUser())));
+                case FRIENDS ->
+                        connection.sendPacket(new ServerListResponsePacket(server.getFriendsHandler().getFriends(connection.getUser())));
+                case GROUPS ->
+                        connection.sendPacket(new ServerListResponsePacket(server.getGroupsHandler().getGroups(connection.getUser(), true), 1337));
+                case MESSAGES ->
+                        connection.sendPacket(new ServerListResponsePacket(server.getGroupsHandler().getMessages(packet.getGroupName(), connection.getUser()), packet.getGroupName()));
+                default -> {
+                    connection.sendPacket(new ServerResponsePacket("Unknown list request type", ServerResponsePacket.ResponseType.ERROR));
+                    log.error("Unknown list type {}", packet.getType());
+                }
+            }
+        }
+        else if (msg instanceof ClientActionPacket packet) {
+
+        }
     }
 
     public ConvoConnection getConnection(ConvoUser user)
@@ -134,5 +155,13 @@ public class ConvoServerHandler extends ConvoHandler {
                 .filter(c -> c.getUser().getInternalId() == user.getInternalId())
                 .findFirst()
                 .orElse(null);
+    }
+
+    public List<ConvoUser> getUsers() {
+        return connections.stream()
+                .filter(ConvoConnection::isAuthFinished)
+                .map(ConvoConnection::getUser)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 }
